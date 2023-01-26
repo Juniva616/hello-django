15f891b3fef4bfb165646cc6e3abacdd7edde02e
Juniva616 <juniva616@gmail.com>
2023-01-26 10:58:56 +0000

    feat: add django form functional

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from todo.views import get_todo_list, add_item
+ 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Item
+from .forms import ItemForm
+
 
 # Create your views here.
 
@@ -14,9 +16,13 @@ def get_todo_list(request):
 
 def add_item(request):
     if request.method == "POST":
-        name = request.POST.get('item_name')
-        done = 'done' in request.POST
-        Item.objects.create(name=name, done=done)
+         form = ItemForm(request.POST)
+         if form.is_valid():
+            form.save()
+            return redirect('get_todo_list')
+    form = ItemForm()
+    context = {
+        'form': form
+    }    
+    return render(request, 'todo/add_item.html', context)
 
-        return redirect('get_todo_list')
-    return render(request, 'todo/add_item.html')  
